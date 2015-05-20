a4bd6c8b7d9bf19e81969f5d37613b24f721c50c
Joanne Zanetton <joannezanetton@gmail.com>
2015-05-20 12:29:05 +0100

    sign in added

--- app/server.rb
+++ app/server.rb
@@ -57,6 +57,22 @@ class BookmarkManager < Sinatra::Base
     else
       flash.now[:errors] = @user.errors.full_messages
       erb :'users/new'
+    end
   end
+      get '/sessions/new' do
+      erb :'sessions/new'
+    end
+    
+  post '/sessions' do
+    email, password = params[:email], params[:password]
+    user = User.authenticate(email, password)
+    if user
+      session[:user_id] = user.id
+      redirect to('/')
+    else
+      flash[:errors] = ['The email or password is incorrect']
+      erb :'sessions/new'
+    end
   end
+
 end
