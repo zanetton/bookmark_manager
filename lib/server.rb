3c673d0495f7530bad2ee0db987ff06177c5b14b
Joanne Zanetton <joannezanetton@gmail.com>
2015-05-20 11:21:06 +0100

    handling input errors

--- lib/server.rb
+++ lib/server.rb
@@ -68,7 +68,7 @@ class BookmarkManager < Sinatra::Base
       session[:user_id] = @user.id
       redirect to('/')
     else
-      flash[:notice] = 'Sorry, your passwords do not match'
+      flash.now[:errors] = @user.errors.full_messages
       erb :'users/new'
   end
   end
--- lib/user.rb
+++ lib/user.rb
@@ -8,7 +8,7 @@ class User
   attr_accessor :password_confirmation
 
   property :id, Serial
-  property :email, String, unique: true
+  property :email, String, unique: true, message: 'This email is already taken'
   # this will store both the password and the salt
   # It's Text and not String because String holds
   # 50 characters by default
